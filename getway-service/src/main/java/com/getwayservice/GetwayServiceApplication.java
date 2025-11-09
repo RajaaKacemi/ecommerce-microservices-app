87899c2b53331299b6d0552b59ef4e43a51e4348
RajaaKacemi <rajaa01.kacemi@gmail.com>
2025-11-09 12:06:13 +0100

    make routes dynamique

--- getway-service/src/main/java/com/getwayservice/GetwayServiceApplication.java
+++ getway-service/src/main/java/com/getwayservice/GetwayServiceApplication.java
@@ -2,12 +2,34 @@ package com.getwayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GetwayServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GetwayServiceApplication.class, args);
+    }
+
+//    @Bean
+    public RouteLocator routes(RouteLocatorBuilder builder){
+        return builder.routes()
+                .route("r1", predicate->predicate.path("/customers/**")
+                        .uri("lb://CUSTOMER-SERVICE"))
+                .route("r2", predicate->predicate.path("/products/**")
+                        .uri("lb://INVENTORY-SERVICE"))
+                .build();
+    }
+
+    @Bean
+    public DiscoveryClientRouteDefinitionLocator dynamicRoutes(ReactiveDiscoveryClient rdc,
+                                                               DiscoveryLocatorProperties dlp){
+        return new DiscoveryClientRouteDefinitionLocator(rdc , dlp);
     }
 
 }
